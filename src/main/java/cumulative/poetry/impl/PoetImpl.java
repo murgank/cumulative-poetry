ae07f757a0eebfc2b75d40e09f9091d858ec59aa
Murgank Kumar <murgankkumar@INmurgankkumar.local>
2019-05-09 15:24:14 +0530

    Release 2

--- src/main/java/cumulative/poetry/impl/PoetImpl.java
+++ src/main/java/cumulative/poetry/impl/PoetImpl.java
@@ -1,20 +1,40 @@
 package cumulative.poetry.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cumulative.poetry.Poet;
 
 public class PoetImpl {
 
 	public static void main(String[] args) {
 		Poet poet;
+		boolean echoFlag = false;
+		List<String> argsList = new ArrayList<String>();
+		for (String arg : args) {
+			argsList.add(arg);
+		}
 		try {
-			if (args[0].equals("--reveal-for-day")) {
+			if (argsList.contains("--echo")) {
+				echoFlag = true;
+			}
+			if (argsList.contains("--reveal-for-day")) {
 				poet = new Poet();
-				System.out.println(poet.recite(Integer.parseInt(args[1])));
-			} else if (args[0].equals("--recite")) {
+				int index = argsList.indexOf("--reveal-for-day");
+				if (echoFlag) {
+					System.out.println(poet.echoRecite(Integer.parseInt(args[index + 1])));
+				} else {
+					System.out.println(poet.recite(Integer.parseInt(args[index + 1])));
+				}
+			} else if (argsList.contains("--recite")) {
 				for (int i = 1; i <= 12; i++) {
 					poet = new Poet();
 					System.out.println("Day " + i + " -");
-					System.out.println(poet.recite(i));
+					if (echoFlag) {
+						System.out.println(poet.echoRecite(i));
+					} else {
+						System.out.println(poet.recite(i));
+					}
 					System.out.println("");
 				}
 			}
@@ -22,7 +42,7 @@ public class PoetImpl {
 			System.out.println("Please provide command line args");
 		} catch (NumberFormatException e) {
 			System.out.println("Number invalid");
-		} catch (Exception e){
+		} catch (Exception e) {
 			System.out.println("Genral Exception encountered");
 		}
 	}
